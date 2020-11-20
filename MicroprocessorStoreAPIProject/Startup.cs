4c99715566aba7adc731ec5527f4b83cc448284a
alanquach8 <alanquach2000@hotmail.com>
2020-11-20 16:44:15 -0500

    configured swagger and deleted unnecessary files

--- MicroprocessorStoreAPIProject/Startup.cs
+++ MicroprocessorStoreAPIProject/Startup.cs
@@ -34,11 +34,20 @@ namespace MicroprocessorStoreAPIProject
             services.AddScoped<IMicroprocessorStoreRepository, MicroprocessorStoreRepository>();
 
             services.AddAutoMapper();
+
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c => 
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Microprocessor Store API");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
