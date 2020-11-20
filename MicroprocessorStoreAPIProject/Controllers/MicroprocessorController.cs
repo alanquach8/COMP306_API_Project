d3bbd6fde38c4d05b550c6290ba5c0d8fc4e28a4
alanquach8 <alanquach2000@hotmail.com>
2020-11-19 19:03:15 -0500

    creates routes for getting all stores/microprocessors, or by ID

--- MicroprocessorStoreAPIProject/Controllers/MicroprocessorController.cs
+++ MicroprocessorStoreAPIProject/Controllers/MicroprocessorController.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using MicroprocessorStoreAPIProject.Models;
+using MicroprocessorStoreAPIProject.Services;
+using MicroprocessorStoreLibrary;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +15,41 @@ namespace MicroprocessorStoreAPIProject.Controllers
     [ApiController]
     public class MicroprocessorController : ControllerBase
     {
+        private IMicroprocessorStoreRepository _microprocessorStoreRepository;
+        private readonly IMapper _mapper;
+
+        public MicroprocessorController(IMicroprocessorStoreRepository microprocessorStoreRepository, IMapper mapper)
+        {
+            _microprocessorStoreRepository = microprocessorStoreRepository;
+            _mapper = mapper;
+        }
+
+        // GET: /GetMicroprocessors
+        [HttpGet]
+        [Route("GetMicroprocessors")]
+        public async Task<ActionResult<Microprocessor>> GetMicroprocessors()
+        {
+            var microprocessorEntities = await _microprocessorStoreRepository.GetMicroprocessors();
+
+            var results = _mapper.Map<IEnumerable<MicroprocessorDto>>(microprocessorEntities);
+
+            return Ok(results);
+        }
+
+        // GET /GetMicroprocessor/{id}
+        [HttpGet]
+        [Route("GetMicroprocessor/{id}")]
+        public async Task<ActionResult<Store>> GetMicroprocessorById(int id)
+        {
+            var microprocessor = await _microprocessorStoreRepository.GetMicroprocessorById(id);
+
+            if (microprocessor == null)
+            {
+                return NotFound();
+            }
+
+            var microprocessorResult = _mapper.Map<MicroprocessorDto>(microprocessor);
+            return Ok(microprocessorResult);
+        }
     }
 }
--- MicroprocessorStoreAPIProject/Controllers/StoreController.cs
+++ MicroprocessorStoreAPIProject/Controllers/StoreController.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using MicroprocessorStoreAPIProject.Models;
+using MicroprocessorStoreAPIProject.Services;
+using MicroprocessorStoreLibrary;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +15,41 @@ namespace MicroprocessorStoreAPIProject.Controllers
     [ApiController]
     public class StoreController : ControllerBase
     {
+        private IMicroprocessorStoreRepository _microprocessorStoreRepository;
+        private readonly IMapper _mapper;
+
+        public StoreController(IMicroprocessorStoreRepository microprocessorStoreRepository, IMapper mapper)
+        {
+            _microprocessorStoreRepository = microprocessorStoreRepository;
+            _mapper = mapper;
+        }
+
+        // GET: /GetStores
+        [HttpGet]
+        [Route("GetStores")]
+        public async Task<ActionResult<Store>> GetStores()
+        {
+            var storeEntities = await _microprocessorStoreRepository.GetStores();
+
+            var results = _mapper.Map<IEnumerable<StoreDto>>(storeEntities);
+
+            return Ok(results);
+        }
+
+        // GET /GetStore/{id}
+        [HttpGet]
+        [Route("GetStore/{id}")]
+        public async Task<ActionResult<Store>> GetStoreById(int id)
+        {
+            var store = await _microprocessorStoreRepository.GetStoreById(id);
+
+            if (store == null)
+            {
+                return NotFound();
+            }
+
+            var storeResult = _mapper.Map<StoreDto>(store);
+            return Ok(storeResult);
+        }
     }
 }
