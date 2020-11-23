ff8da1f74a5b548d0821fdb6aaab30bbbd5e8bf3
alanquach8 <alanquach2000@hotmail.com>
2020-11-23 14:14:38 -0500

    added front end client project

---
+++ MicroprocessorStoreClient/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MicroprocessorStoreClient.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
